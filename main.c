fbefaf0c20b907290cc732da37686d8ff287fceb
Jonas G <54191355+TerritorialBreak5@users.noreply.github.com>
2021-12-20 15:13:20 +0000

    Update main.c

--- main.c
+++ main.c
@@ -11,14 +11,17 @@ void Troca(int *i, int *j){
 int main() {
 	int a;
 	int b;
+	
 	printf("Introduza o valor do 1º: ");
 	scanf("%d",&a);
+	
 	fflush(stdin);
    
 	printf("Introduza o valor do 2º: ");
 	scanf("%d",&b);
 	
 	Troca(&a ,&b);
+	
 	printf("Valor do 1º: %d \n", a);
 	printf("Valor do 2º: %d \n", b);
 }
